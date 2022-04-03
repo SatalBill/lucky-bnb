db72374bae065dbd2c028c3f5fb1caaacf0fe777
SatalBill <satel1217@163.com>
2022-04-03 12:56:46 +0800

    check network, draw, mywalletIsListed

--- src/layouts/Hero/Hero.js
+++ src/layouts/Hero/Hero.js
@@ -1,13 +1,144 @@
 import React from "react";
+import styled from 'styled-components'
 import styles from "./Hero.module.css";
 import luckyBnbHeroBanner from "assets/images/luckyBnbHeroBanner.png";
 import { Fade } from "react-awesome-reveal";
+import Web3 from 'web3'
+import Button from "components/Button/Button";
+
+const DrawButton = styled.div`
+  padding: 10px 1rem;
+  background: linear-gradient(90deg, #8D0C0B 0%, #FF5E28 107.58%);
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  margin: 0 1em;
+  color: white;
+  &:hover {
+    filter: brightness(1.2);
+  }
+  &:active {
+    transform: translateY(1px);
+  }
+`
+const CheckButton = styled(DrawButton)`
+  border: 2px solid transparent;
+
+  background: linear-gradient(black, black) padding-box,
+    linear-gradient(
+        130deg,
+        rgba(232, 178, 53, 0.68),
+        transparent,
+        rgba(232, 178, 53, 0.68)
+      )
+      border-box;
+  &:hover {
+    background: linear-gradient(black, black) padding-box,
+    linear-gradient(
+        -20deg,
+        rgba(232, 178, 53, 0.68),
+        transparent,
+        rgba(232, 178, 53, 0.68)
+      )
+      border-box;
+  }
+`
 
 function Hero(props) {
-  console.log('=======^^^', props.prizePool)
-  console.log('=======^^^', typeof(props.prizePool))
+  const [val, setVal] = React.useState("")
   const cutDigits = (num) => {
     return num.toString().substring(0, 5)
+  }
+  let sendObj
+  const loadAddress = () => {
+    if (props.address == "") {
+      sendObj = {
+        type: 'info',
+        msg: "Please connect wallet."
+      }
+      props.toast(sendObj)
+    } else {
+      console.log(props.address)
+      setVal(props.address)
+    }
+  }
+  const handleCheckIsInPool = async () => {
+    try {
+      if (val == "") {
+        sendObj = {
+          type: 'info',
+          msg: "Please load your wallet address."
+        }
+        props.toast(sendObj)
+      } else {
+        const web3 = window.web3
+        const isValidAddress = web3.utils.isAddress(val)
+        if (!isValidAddress) {
+          sendObj = {
+            type: 'warning',
+            msg: "Please input correct address."
+          }
+          props.toast(sendObj)
+          console.log('val', val)
+          console.log('val2', props.address)
+        } else {
+          props.isLoading(true)
+          const luckyContract = props.contractObj
+          const isListed = await luckyContract.methods.isInThePool(props.address).call()
+
+          console.log('isListed=>', isListed)
+          props.isLoading(false)
+          if (isListed) {
+            sendObj = {
+              type: 'success',
+              msg: "You have already listed."
+            }
+          }
+          else {
+            sendObj = {
+              type: 'warning',
+              msg: "Your address is not listed on the pool yet."
+            }
+          }
+          props.toast(sendObj)
+        }
+        console.log('isValidAddress', isValidAddress)
+      }
+    } catch (error) {
+      sendObj = {
+        type: 'success',
+        msg: "You have already listed."
+      }
+      props.toast(sendObj)
+    }
+
+  }
+
+  const handleDraw = async () => {
+    try {
+      props.isLoading(true)
+      const luckyContract = props.contractObj
+      const res = await luckyContract.methods.draw().call()
+      props.isLoading(false)
+      if (res) {
+        sendObj = {
+          type: 'success',
+          msg: "Success"
+        }
+        props.toast(sendObj)
+      }
+      console.log('res', res)
+    } catch (error) {
+      props.isLoading(false)
+      sendObj = {
+        type: 'error',
+        msg: "Not a time yet, wait until next drawing timeslot"
+      }
+      props.toast(sendObj)
+      console.log('draw error=>', error)
+    }
   }
   return (
     <div className={styles.hero}>
@@ -27,8 +158,20 @@ function Hero(props) {
                 <span className="weight-7">8</span> */}
               </div>
             </div>
+            <div className={styles.input_wrapper}>
+              <input
+                className={styles.wallet_input}
+                value={val}
+                onChange={(e) => setVal(e.target.value)}
+                placeholder="Your wallet address..."
+              />
+              <Button title="Load" onClick={loadAddress} />
+            </div>
+            <div className={styles.input_wrapper}>
+              <CheckButton onClick={handleCheckIsInPool}>Check my address</CheckButton>
+              <DrawButton onClick={handleDraw}>Draw</DrawButton>
+            </div>
           </div>
-          
         </div>
       </Fade>
     </div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Web3 from 'web3'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Coins from "layouts/Coins/Coins";
 import Contact from "layouts/Contact/Contact";
 import Features from "layouts/Features/Features";
@@ -11,44 +13,110 @@ import Introducing from "layouts/Introducing/Introducing";
 import Navbar from "layouts/Navbar/Navbar";
 import Tokenomics from "layouts/Tokenomics/Tokenomics";
 import UntilNextDraw from "layouts/UntilNextDraw/UntilNextDraw";
+import PageLoader from "components/Loader/PageLoader"
 import styles from "./HomePage.module.css";
 import { Fade } from "react-awesome-reveal";
 import luckyBnbAbi from "abi/luckyBnbAbi.json"
 
 const LuckyBnbAddress = "0x18ec883176b8809a807c7ba14ac09f5ef18fdf04"
+declare var window: any
 
 function HomePage(props: any) {
   const [isLoading, setIsLoading] = useState(false)
   const [prizePool, setPrizePool] = useState('0')
+  const [contractObj, setContractObj] = useState(null)
 
   useEffect(() => {
     const start = async () => {
-      console.log('start====');
-
       try {
         setIsLoading(true)
         window.web3 = new Web3("https://bsc-dataseed.binance.org/")
         await window.ethereum.enable()
         const web3 = window.web3
         const luckyBnbContracts = new web3.eth.Contract(luckyBnbAbi, LuckyBnbAddress);
+        setContractObj(luckyBnbContracts)
 
         // contract balance
         const prizePoolInWei = await web3.eth.getBalance(LuckyBnbAddress)
         const prizePoolTemp = web3.utils.fromWei(prizePoolInWei);
         console.log('prizePoolTemp==>>', prizePoolTemp)
-        console.log('typeof_prizePoolTemp==>>', typeof(prizePoolTemp))
         setPrizePool(prizePoolTemp)
 
+        // check network
+        const chainId = 56 // BSC Mainnet
+        if (window.ethereum.networkVersion !== chainId) {
+          try {
+            await window.ethereum.request({
+              method: 'wallet_switchEthereumChain',
+              params: [{ chainId: web3.utils.toHex(chainId) }],
+            });
+          } catch (err: any) {
+            // This error code indicates that the chain has not been added to MetaMask.
+            if (err.code === 4902) {
+              await window.ethereum.request({
+                method: 'wallet_addEthereumChain',
+                params: [
+                  {
+                    chainName: 'BSC Mainnet',
+                    chainId: web3.utils.toHex(chainId),
+                    nativeCurrency: { name: 'BNB', decimals: 18, symbol: 'BNB' },
+                    rpcUrls: ['https://bsc-dataseed.binance.org/'],
+                  },
+                ],
+              });
+            }
+          }
+        }
+
+        setIsLoading(false)
       } catch (error) {
+        setIsLoading(false)
         console.log('err==>>', error)
+        // toast('Please check your internet connection.', {
+        //   position: "top-right",
+        //   autoClose: 5000,
+        //   hideProgressBar: false,
+        //   closeOnClick: true,
+        //   pauseOnHover: true,
+        //   draggable: true,
+        //   progress: undefined,
+        // });
       }
 
     }
     start()
   }, [])
 
+  const showToast = (obj: any) => {
+    const option: any = {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    }
+    if (obj.type === 'info') toast.info(`${obj.msg}`, option);
+    else if (obj.type === 'warning') toast.warn(`${obj.msg}`, option);
+    else if (obj.type === 'success') toast.success(`${obj.msg}`, option);
+    else if (obj.type === 'error') toast.error(`${obj.msg}`, option);
+  }
+
   return (
     <div className={styles.home}>
+      {isLoading && <PageLoader />}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Navbar
         connect={props.connect}
         connected={props.connected}
@@ -57,7 +125,13 @@ function HomePage(props: any) {
         killSession={props.killSession}
       />
       <div className="mb-100px" id="home">
-        <Hero prizePool={prizePool} />
+        <Hero
+          prizePool={prizePool}
+          address={props.address}
+          contractObj={contractObj}
+          isLoading={(e: boolean) => setIsLoading(e)}
+          toast={(e: any) => showToast(e)}
+        />
       </div>
       <div className="mb-100px">
         <Fade delay={500} duration={1000} cascade triggerOnce direction="right">
